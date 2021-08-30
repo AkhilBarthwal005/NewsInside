e48383fffa65045d86e2ac7327c11de239257363
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2021-08-30 17:53:59 +0530

    Bootstrap card is added

--- src/Components/NewsComponent.js
+++ src/Components/NewsComponent.js
@@ -4,9 +4,19 @@ import NewsItem from "./NewsItem";
 export default class NewsComponent extends Component {
   render() {
     return (
-      <div>
-        This is news Component.
-        <NewsItem />
+      <div className="container my-2">
+        <h1>NewsInside - Top HeadLines</h1>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="My title" description="My description" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="My title" description="My description" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="My title" description="My description" />
+          </div>
+        </div>
       </div>
     );
   }
