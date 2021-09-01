10e668770bef282546b1931bd2ad6a81d0894255
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2021-09-01 21:21:55 +0530

    dynamic news api is added

--- src/Components/NewsComponent.js
+++ src/Components/NewsComponent.js
@@ -50,6 +50,17 @@ export default class NewsComponent extends Component {
     };
   }
 
+  async componentDidMount() {
+    // this is function which run after the render method automatically and we are making it async.
+    console.log("cdm method");
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=4b32f83d132f4a188e704ad96a6575e2";
+    let data = await fetch(url);
+    let parseData = await data.json();
+    // console.log(parseData);
+    this.setState({ article: parseData.articles });
+  }
+
   render() {
     return (
       <div className="container my-2">
@@ -62,11 +73,17 @@ export default class NewsComponent extends Component {
               <div className="col-md-4" key={element.url}>
                 <NewsItem
                   title={
-                    element.title.length > 45
-                      ? element.title.slice(0, 45) + "..."
-                      : element.title
+                    element.title
+                      ? element.title.length > 45
+                        ? element.title.slice(0, 45) + "..."
+                        : element.title
+                      : "Title missing"
                   }
-                  description={element.description.slice(0, 100)}
+                  description={
+                    element.description
+                      ? element.description.slice(0, 100)
+                      : "Description is missing"
+                  }
                   imageUrl={element.urlToImage}
                   newsUrl={element.url}
                 />
