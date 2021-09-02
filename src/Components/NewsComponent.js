5d18e73d9a38c48773eeb0a9e70e72bc94c90ecd
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2021-09-02 17:46:56 +0530

    next and previous button is added

--- src/Components/NewsComponent.js
+++ src/Components/NewsComponent.js
@@ -10,50 +10,103 @@ export default class NewsComponent extends Component {
       // here we are using state.
       article: [],
       loading: false,
+      page: 1,
+      pageSize: 20,
+      totalResult: 0,
     };
   }
 
   async componentDidMount() {
     // this is function which run after the render method automatically and we are making it async.
     console.log("cdm method");
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=4b32f83d132f4a188e704ad96a6575e2";
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=4b32f83d132f4a188e704ad96a6575e2&page=1&pageSize=20`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    // console.log(parseData);
+    this.setState({
+      article: parseData.articles,
+      totalResult: parseData.totalResults,
+    });
+  }
+
+  loadNews = async (page) => {
+    console.log("load method");
+    // console.log(this.state.page);
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=4b32f83d132f4a188e704ad96a6575e2&page=${page}&pageSize=20`;
     let data = await fetch(url);
     let parseData = await data.json();
     // console.log(parseData);
     this.setState({ article: parseData.articles });
-  }
+  };
+
+  handlePrevious = () => {
+    console.log("Previous page");
+    this.loadNews(this.state.page - 1);
+    this.setState({
+      page: this.state.page - 1,
+      pageSize: this.state.pageSize - 20,
+    });
+    console.log(this.state.page);
+  };
+  handleNext = () => {
+    console.log("Next page");
+    console.log(this.state.page);
+    this.loadNews(this.state.page + 1);
+    this.setState({
+      page: this.state.page + 1,
+      pageSize: this.state.pageSize + 20,
+    });
+  };
 
   render() {
     return (
-      <div className="container my-2">
-        <h1>NewsInside - Top HeadLines</h1>
-        <div className="row">
-          {/* we are using javascript higher order method called map and iterate over array */}
-          {this.state.article.map((element) => {
-            // console.log(element);
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={
-                    element.title
-                      ? element.title.length > 45
-                        ? element.title.slice(0, 45) + "..."
-                        : element.title
-                      : "Title missing"
-                  }
-                  description={
-                    element.description
-                      ? element.description.slice(0, 100)
-                      : "Description is missing"
-                  }
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
-          ;
+      <div>
+        <div className="container my-2">
+          <h1>NewsInside - Top HeadLines</h1>
+          <div className="row">
+            {/* we are using javascript higher order method called map and iterate over array */}
+            {this.state.article.map((element) => {
+              // console.log(element);
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={
+                      element.title
+                        ? element.title.length > 45
+                          ? element.title.slice(0, 45) + "..."
+                          : element.title
+                        : "Title missing"
+                    }
+                    description={
+                      element.description
+                        ? element.description.slice(0, 100)
+                        : "Description is missing"
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark my-3"
+            onClick={this.handlePrevious}
+          >
+            &larr; Previous
+          </button>
+          <button
+            disabled={this.state.pageSize >= this.state.totalResult}
+            type="button"
+            className="btn btn-dark my-3"
+            onClick={this.handleNext}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -5,16 +5,35 @@ export default class NewsItem extends Component {
     let { title, description, imageUrl, newsUrl } = this.props; // here we are deframing the array which passed as props and taking title and description from it.
 
     return (
-      <div className="card my-2" style={{ width: "18rem" }}>
+      <div
+        className="card my-2"
+        style={{
+          width: "18rem",
+          backgroundColor: "#04021f",
+          color: "white",
+          border: "2px solid white",
+        }}
+      >
         <img
-          src={imageUrl ? imageUrl : "Image is missing"}
+          src={
+            imageUrl
+              ? imageUrl
+              : "https://www.socialventurepartners.org/boulder-county/wp-content/themes/svpi/img/defaults/default-svp_news.jpg"
+          }
           className="card-img-top"
           alt="..."
+          height="150px"
+          width="300px"
         />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}</p>
-          <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn-dark"
+          >
             Read more
           </a>
         </div>
