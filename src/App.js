495fc9296b6a0e31e33fc87909c2566ce6b8211d
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2021-09-02 17:48:35 +0530

    footer is added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./Components/Navbar";
 import NewsComponent from "./Components/NewsComponent";
+import Footer from "./Components/Footer";
 
 export default class App extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class App extends Component {
       <div>
         <Navbar />
         <NewsComponent />
+        <Footer />
       </div>
     );
   }
