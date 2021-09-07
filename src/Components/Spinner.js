b91aa1cefb3ada209d2f604ff911d7247f2cde86
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2021-09-07 15:02:56 +0530

    modify spinner

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 export default class Spinner extends Component {
   render() {
     return (
-      <div className="d-flex justify-content-center position-absolute top-50 start-50 translate-middle">
+      <div className="d-flex justify-content-center position-absolute top-40 start-50 translate-middle">
         <div className="spinner-grow text-primary" role="status">
           <span className="visually-hidden">Loading...</span>
         </div>
