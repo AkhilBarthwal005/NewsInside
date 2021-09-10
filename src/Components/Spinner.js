6679eb08b1178b459e64db9fa3660992289e86e1
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2021-09-10 10:44:29 +0530

    Spinner updated

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,32 +1,10 @@
 import React, { Component } from "react";
+import Loading from "./img/ajax-loader.gif";
 export default class Spinner extends Component {
   render() {
     return (
-      <div className="d-flex justify-content-center position-absolute top-40 start-50 translate-middle">
-        <div className="spinner-grow text-primary" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-grow text-secondary" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-grow text-success" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-grow text-danger" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-grow text-warning" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-grow text-info" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-grow text-light" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        <div className="spinner-grow text-dark" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
+      <div className="text-center my-3">
+        <img src={Loading} alt="" />
       </div>
     );
   }
