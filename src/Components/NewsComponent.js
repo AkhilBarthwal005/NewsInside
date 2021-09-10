54e934b72f6771888ada65c496b9669a2b463099
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2021-09-10 10:45:17 +0530

    InfiniteScroll added

--- src/Components/NewsComponent.js
+++ src/Components/NewsComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class NewsComponent extends Component {
   static defaultProps = {
@@ -35,102 +36,85 @@ export default class NewsComponent extends Component {
   };
 
   async componentDidMount() {
-    this.loadNews(this.state.page);
+    console.log("cmd");
+    this.loadNews();
   }
 
-  loadNews = async (page) => {
+  loadNews = async () => {
     console.log("load method");
     // console.log(this.state.page);
     this.setState({ loading: true });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4b32f83d132f4a188e704ad96a6575e2&page=${page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4b32f83d132f4a188e704ad96a6575e2&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parseData = await data.json();
     console.log(parseData);
     this.setState({
-      article: parseData.articles,
+      article: this.state.article.concat(parseData.articles),
       totalResult: parseData.totalResults,
       loading: false,
     });
   };
 
-  handlePrevious = async () => {
-    console.log("Previous page");
-    // this.setState({ loading: true });
-    this.loadNews(this.state.page - 1);
-    this.setState({
-      page: this.state.page - 1,
-    });
-    console.log(this.state.page);
-  };
-  handleNext = async () => {
-    console.log("Next page");
-    // this.setState({ loading: true });
-    console.log(this.state.page);
-    this.loadNews(this.state.page + 1);
-    this.setState({
-      page: this.state.page + 1,
-    });
+  fetchMoreData = () => {
+    console.log("fetchmore data");
+    this.setState({ page: this.state.page + 1 });
+    this.loadNews();
   };
 
   render() {
     return (
       <div>
-        <div className="container my-2">
+        <div>
           <h1 className="text-center my-4">
             NewsInsider - Top {this.capitalize(this.props.category)} HeadLines
           </h1>
-          {this.state.loading && <Spinner />}
-          <div className="row">
-            {/* we are using javascript higher order method called map and iterate over array */}
-            {!this.state.loading &&
-              this.state.article.map((element) => {
-                // console.log(element);
-                return (
-                  <div className="col-md-4" key={element.url}>
-                    <NewsItem
-                      title={
-                        element.title
-                          ? //element.title.length > 45
-                            //? element.title.slice(0, 45) + "..."
-                            element.title
-                          : "Title missing"
-                      }
-                      description={
-                        element.description
-                          ? element.description.slice(0, 250) + "..."
-                          : "Description is missing"
-                      }
-                      imageUrl={element.urlToImage}
-                      newsUrl={element.url}
-                      author={element.author}
-                      publishedAt={element.publishedAt}
-                      source={element.source.name}
-                    />
-                  </div>
-                );
-              })}
-          </div>
-        </div>
-        <div className="container d-flex justify-content-between">
-          <button
-            disabled={this.state.page <= 1}
-            type="button"
-            className="btn btn-dark my-3"
-            onClick={this.handlePrevious}
-          >
-            &larr; Previous
-          </button>
-          <button
-            disabled={
-              this.state.page + 1 >=
+          {/* {this.state.loading && <Spinner />} */}
+
+          <InfiniteScroll
+            dataLength={this.state.article.length}
+            next={this.fetchMoreData}
+            hasMore={
+              this.state.page !==
               Math.ceil(this.state.totalResult / this.props.pageSize)
             }
-            type="button"
-            className="btn btn-dark my-3"
-            onClick={this.handleNext}
+            loader={<Spinner />}
           >
-            Next &rarr;
-          </button>
+            <div className="container">
+              <div className="row">
+                {/* we are using javascript higher order method called map and iterate over array */}
+                {/* {console.log(this.state.page)}
+                {console.log(
+                  Math.ceil(this.state.totalResult / this.props.pageSize)
+                )} */}
+                {this.state.article.map((element) => {
+                  // console.log(element);
+                  return (
+                    <div className="col-md-4" key={element.url}>
+                      <NewsItem
+                        title={
+                          element.title
+                            ? //element.title.length > 45
+                              //? element.title.slice(0, 45) + "..."
+                              element.title
+                            : "Title missing"
+                        }
+                        description={
+                          element.description
+                            ? element.description.slice(0, 250) + "..."
+                            : "Description is missing"
+                        }
+                        imageUrl={element.urlToImage}
+                        newsUrl={element.url}
+                        author={element.author}
+                        publishedAt={element.publishedAt}
+                        source={element.source.name}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </InfiniteScroll>
         </div>
       </div>
     );
